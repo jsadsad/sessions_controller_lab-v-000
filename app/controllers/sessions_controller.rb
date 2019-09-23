f48f8d2634f1d7ecbbf1817f27838d93e3082de5
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-23 20:52:29 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       redirect_to :login
     else
       session[:name] = params[:name]
-      redirect_to '/'
+      redirect_to :root
     end
   end
 
